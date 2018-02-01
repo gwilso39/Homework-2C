68a2629bb547693dd952ad86fe21c74be0fb826b
Greg Wilson <greg.wilson5@outlook.com>
2018-01-31 22:34:34 -0500

    complete homework assignment

--- Homework 2C/Program.cs
+++ Homework 2C/Program.cs
@@ -10,6 +10,33 @@ namespace Homework_2C
     {
         static void Main(string[] args)
         {
+            //Syntactic sugar is meant to make life easier for a programmer or anyone
+            //trying to read code.  It simplifies the process by making things a bit
+            //more clear or concise and in an alternative style.  The language is said
+            //to be 'sweeter' for human use.
+        }
+
+        string RVProblems(string trailer = "Hideout", string wheels = "four")
+        {
+            Console.WriteLine($"A {trailer} has {wheels} wheels.");
+            return "";
+        }
+    
+        //equivalent...
+        string MoreRVProblems(string trailer, string wheels)
+        {
+            Console.WriteLine($"A {trailer} has {wheels} wheels.");
+            return "";
+        }
+
+        string MoreRVProblems(string trailer)
+        {
+            return MoreRVProblems(trailer, "four");
+        }
+
+        string MoreRVProblems()
+        {
+            return MoreRVProblems("Hideout");
         }
     }
 }
